da591766e6188e95440509e4ccfd02151cc0669b
Lucas-TA <dev.ascencao@gmail.com>
2024-01-31 17:04:50 -0300

    adding dist file

---
+++ dist/scripts/main.js
@@ -0,0 +1,1 @@
+window.addEventListener("DOMContentLoaded",()=>{var e=document.getElementById("collapseButton");const n=document.getElementById("linksContainer");e.addEventListener("click",()=>{n.classList.toggle("dropdown-animation")})});
